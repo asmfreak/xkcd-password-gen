3f65ad8e3ba8637d3426880ea3187799c97ca3c3
ASM <cpp.create@gmail.com>
2017-10-23 04:13:06 +0200

    Испралено описание, добавлен список частотных русских слов

--- pwgen.py
+++ pwgen.py
@@ -5,13 +5,15 @@ from plumbum.cli import Application, ExistingFile, SwitchAttr
 
 
 class XKCDPasswordGenerator(object):
-    def __init__(self, wordlist_file='russian_.txt'):
-        with open(wordlist_file) as f:
-            self.words = list(map(lambda x:x[:-1], f))
+    def __init__(self, wordlist_files=['russian_.txt']):
+        self.words = []
+        for wordlist_file in wordlist_files:
+            with open(wordlist_file) as f:
+                self.words.extend(map(lambda x:x[:-1], f))
 
-    def generate_password(self):
+    def generate_password(self, wordcount=4):
         pw = []
-        for i in range(4):
+        for i in range(wordcount):
             pw.append(choice(self.words))
         return ' '.join(pw)
 
@@ -23,8 +25,11 @@ def endless():
         pass
 
 class pwgen(Application):
-    wordlist = SwitchAttr(["-w", "--wordlist"], ExistingFile, "russian_.txt")
-    def main(self, n: int = 4):
+    """генератор паролей по методу xkcd, n - количество необходимых паролей, при n=0 генератор будет выдавать ноые пароли по нажатию enter"""
+    wordlist = SwitchAttr(
+        ["-w", "--wordlist"], ExistingFile, "russian_.txt",
+        list = True, help="файл со списком слов для генерации пароля")
+    def main(self, n: int = 0):
         pwgen = XKCDPasswordGenerator(self.wordlist)
         if n != 0:
             iter = range(n)
